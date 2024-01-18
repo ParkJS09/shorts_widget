c99d17859c02ad5ed623d41d33f36570e81840f4
parkjs <python2_7@naver.com>
2024-01-19 00:08:04 +0900

    기본 구조 작성

--- lib/shorts_list_widget.dart
+++ lib/shorts_list_widget.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shorts_widget/data/short_item.dart';
 import 'package:shorts_widget/widget/arrow_button.dart';
-import 'package:shorts_widget/widget/dynamic_center_widget.dart';
 import 'package:shorts_widget/widget/shorts_item_widget.dart';
 
 class ShortsListWidget extends StatefulWidget {
@@ -22,79 +21,89 @@ class _ShortsListWidgetState extends State<ShortsListWidget> {
       width: double.infinity,
       height: double.infinity,
       color: Colors.grey[300],
-      child: Stack(
+      child: Row(
         children: [
-          if (currentItem > 0)
-            Align(
-              alignment: FractionalOffset(0.2, 0.5),
+          Flexible(
+            flex: 3,
+            child: currentItem > 0
+                ? Center(
+                    child: Container(
+                      width: 100,
+                      height: 150,
+                      child: ShortsItemWidget(
+                        isCurrentItem: false,
+                        item: widget.item[currentItem - 1],
+                      ),
+                    ),
+                  )
+                : Container(),
+          ),
+          Center(
+            child: Flexible(
+              flex: 4,
               child: Container(
-                width: 100,
-                child: ShortsItemWidget(
-                  isCurrentItem: false,
-                  item: widget.item[currentItem - 1],
+                width: 300,
+                height: 500,
+                color: Colors.red,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    ArrowButton(
+                      iconData: Icons.navigate_before,
+                      onPressed: () {
+                        if (currentItem > 0) {
+                          setState(() {
+                            currentItem -= 1;
+                          });
+                        } else {
+                          //TODO Show SnackBar Error
+                        }
+                      },
+                    ),
+                    const SizedBox(
+                      width: 14.0,
+                    ),
+                    ShortsItemWidget(
+                      isCurrentItem: true,
+                      item: widget.item[currentItem],
+                    ),
+                    const SizedBox(
+                      width: 14.0,
+                    ),
+                    ArrowButton(
+                      iconData: Icons.navigate_next,
+                      onPressed: () {
+                        if (currentItem + 1 < widget.item.length) {
+                          setState(() {
+                            currentItem += 1;
+                          });
+                        } else {
+                          //TODO Show SnackBar Error
+                        }
+                      },
+                    ),
+                  ],
                 ),
-              ),
-            ),
-          Align(
-            alignment: Alignment.center,
-            child: Container(
-              width: 300,
-              height: 500,
-              color: Colors.red,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  ArrowButton(
-                    iconData: Icons.navigate_before,
-                    onPressed: () {
-                      if (currentItem > 0) {
-                        setState(() {
-                          currentItem -= 1;
-                        });
-                      } else {
-                        //TODO Show SnackBar Error
-                      }
-                    },
-                  ),
-                  const SizedBox(
-                    width: 14.0,
-                  ),
-                  ShortsItemWidget(
-                    isCurrentItem: true,
-                    item: widget.item[currentItem],
-                  ),
-                  const SizedBox(
-                    width: 14.0,
-                  ),
-                  ArrowButton(
-                    iconData: Icons.navigate_next,
-                    onPressed: () {
-                      if (currentItem + 1 < widget.item.length) {
-                        setState(() {
-                          currentItem += 1;
-                        });
-                      } else {
-                        //TODO Show SnackBar Error
-                      }
-                    },
-                  ),
-                ],
               ),
             ),
           ),
+          Flexible(
+            flex: 3,
+            child: currentItem + 1 < widget.item.length
+                ? Center(
+                    child: Container(
+                      width: 100,
+                      height: 150,
+                      child: ShortsItemWidget(
+                        isCurrentItem: false,
+                        item: widget.item[currentItem + 1],
+                      ),
+                    ),
+                  )
+                : Container(),
+          ),
           // 다음 아이템이 있으면 추가
-          if (currentItem + 1 < widget.item.length)
-            Align(
-              alignment: FractionalOffset(0.8, 0.5),
-              child: Container(
-                width: 100,
-                child: ShortsItemWidget(
-                  isCurrentItem: false,
-                  item: widget.item[currentItem + 1],
-                ),
-              ),
-            ),
         ],
       ),
     );
